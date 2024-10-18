902a1ff8a66bd7271173ba059e3e9db072f52fb3
nathanljones <nathan.l.jones+github@protonmail.com>
2024-10-18 10:02:51 +0100

    day 4 pt 1 - add more tests & use iterators

--- day_04/src/bin/part_01.rs
+++ day_04/src/bin/part_01.rs
@@ -7,23 +7,19 @@ fn main() {
 fn count_winning_nos_for_line(line: &str) -> u32 {
     let replaced_line = line.replace(":", "|");
     let split_lines: Vec<&str> = replaced_line.split('|').collect();
-    let mut count: u32 = 0;
     let winning_nos: Vec<u32> = get_card_numbers(&split_lines[1]);
     let numbers_to_find: Vec<u32> = get_card_numbers(&split_lines[2]);
 
-    for number in winning_nos {
-        if numbers_to_find.contains(&number) {
-            count += 1;
-        }
-    }
-    count
+    numbers_to_find
+        .iter()
+        .filter(|number| winning_nos.contains(number))
+        .count() as u32
 }
 fn get_card_numbers(numbers: &str) -> Vec<u32> {
-    let ret_val: Vec<u32> = numbers
+    numbers
         .split_whitespace()
         .map(|x| x.parse::<u32>().unwrap())
-        .collect();
-    ret_val
+        .collect()
 }
 
 fn calculate_no_points(number: u32) -> u32 {
@@ -63,9 +59,34 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         println!("{:?}", res);
     }
     #[test]
-    fn number_winning_no_for_line() {
+    fn number_winning_no_for_card1() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
         assert_eq!(count_winning_nos_for_line(input), 4)
+    }
+    #[test]
+    fn number_winning_no_for_card2() {
+        let input = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19";
+        assert_eq!(count_winning_nos_for_line(input), 2)
+    }
+    #[test]
+    fn number_winning_no_for_card3() {
+        let input = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1";
+        assert_eq!(count_winning_nos_for_line(input), 2)
+    }
+    #[test]
+    fn number_winning_no_for_card4() {
+        let input = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83";
+        assert_eq!(count_winning_nos_for_line(input), 1)
+    }
+    #[test]
+    fn number_winning_no_for_card5() {
+        let input = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36";
+        assert_eq!(count_winning_nos_for_line(input), 0)
+    }
+    #[test]
+    fn number_winning_no_for_card6() {
+        let input = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(count_winning_nos_for_line(input), 0)
     }
     #[test]
     fn calculate_powers() {
